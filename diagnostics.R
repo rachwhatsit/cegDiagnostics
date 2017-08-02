b171ab45a31d198de673bb341fe52a93530b15ab
Rachel Wilkerson <rachelw52@gmail.com>
2017-08-02 10:58:08 +0100

    init commit

---
+++ diagnostics.R
@@ -0,0 +1,42 @@
+library(dplyr);library(ggplot2)
+setwd("/home/rachel/Documents/diagnostics/")
+df<-read.csv(file = "CHDS.latentexample1.csv")
+
+ df %>%
+   group_by(Social, Economic, Events, Admission) %>%
+   tally() -> counts #counts for all pathways in the CEG
+ 
+write.csv(counts,'counts.csv')
+
+get.zhed <- function(prior,n=50) {#dataframes should also be added for the counts
+  #add checks to make sure that prior has same number of items as counts in dataframe
+  Zm <- rep(NA, n)
+  Sm <- rep(NA, n)
+  Em <- rep(NA, n)
+  Vm <- rep(NA, n)
+  p <- rep(NA, n)
+  for (i in 5:n){
+    df_cut <- df[1:i,]
+    df_cut %>%
+      group_by(Social) %>%
+      tally() -> u1 #stage1
+    counts = u1$n 
+    p[i] = sum(gamma(prior))*prod(gamma(prior+counts))/(prod(gamma(prior)) * gamma(sum(prior)+sum(counts)))#prob of observing the sequence
+    #p[i] = sum(gamma(prior))* gamma(prior+counts)/(prod(gamma(prior)) * gamma(sum(prior)+sum(counts)))#prob of observing each iteration
+    #compute the z statistics
+   Sm[i]=-log(p[i])
+   Em[i]=-p[i]*log(p[i])
+   Vm[i]=p[i]*(log(p[i])^2) - (Em[i])^2
+   Zm[i]=sum(na.omit(Sm)) - sum(na.omit(Em)) / sqrt(sum(na.omit(Vm)))
+  }
+  return(list(Sm,Zm))
+}
+
+mod1 <- get.zhed(prior=c(10,1),n=10)
+mod2 <- get.zhed(prior=c(.5,.5),n=10)
+
+plot(mod2[[2]],xlab='Relevant sample size', ylab = 'Cumulative logarithmic penalty')
+lines(mod1[[2]])
+legend(2,25,c("Dir(1,10)", "Dir(6,4)"))
+title("Diagnostics for u1")
+
